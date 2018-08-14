693afb9d207210ccf9432b286c28272873c9d891
Saul Restrepo <saulrestrepo65488@correo.itm.edu.co>
2018-08-14 07:52:47 -0500

    Sales2

---
+++ sales/sales/Services/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using sales.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales.Services
+{
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase, string ff)
+        {
+            //try
+            //{
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw;
+            //}
+        }
+    }
+}
