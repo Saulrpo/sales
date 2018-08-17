83a7fedbc5a650035b371ff98efac15396cfaef5
Saul Restrepo <saulrestrepo65488@correo.itm.edu.co>
2018-08-17 08:24:02 -0500

    Sales2

---
+++ sales/sales/ViewModels/MainViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sales.ViewModels
+{
+    public class MainViewModels
+    {
+        public ProductsViewModels Products { get; set; }
+
+        public MainViewModels()
+        {
+            this.Products = new ProductsViewModels();
+        }
+    }
+}
---
+++ sales/sales/ViewModels/ProductsViewModels.cs
@@ -0,0 +1,41 @@
+﻿namespace sales.ViewModels
+{
+    using Common.Models;
+    using System.Collections.ObjectModel;
+    using Services;
+    using System;
+    using Xamarin.Forms;
+    using System.Collections.Generic;
+
+    public class ProductsViewModels : BaseViewModel
+    {
+        private ApiService ApiService;
+
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModels()
+        {
+            this.ApiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.ApiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Aceptar");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
